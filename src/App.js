7e6d38df9ceae239b6fb73980e16329894a8194b
Mykola Lubenets <teabrutal@gmail.com>
2019-01-10 01:25:20 +0200

    Template for tom row images created

--- src/App.js
+++ src/App.js
@@ -11,11 +11,39 @@ import SettingsBlock from './components/settingsWizard/SettingsBlock';
 import './app.scss'
 
 class App extends Component {
+  state = {
+    photosCount: 0
+  }
+
+  componentDidMount() {
+   this.getPhotosCount();
+   window.addEventListener("resize", this.getPhotosCount);
+  }
+
+  getPhotosCount = (e) => {
+    let w = window.innerWidth;
+    let n = 0;
+    if ( w < 600 ) { //mobile
+      n = 3;
+    } else if ( w > 599 && w < 900 ) { //portrait tablet
+      n = 4;
+    } else if ( w > 899 && w < 1200 ) { //lanscape tablet
+      n = 5;
+    } else if ( w > 1199 && w < 1800 ) { //desktop
+      n = 7;
+    } else { //big desktop
+      n = 8;
+    }
+    this.setState({
+      photosCount: n
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <SettingsBlock />
-        <TopPhotoRow />
+        <TopPhotoRow photosCount={this.state.photosCount} />
         <LanguageSlider />
         <PhotoGallery />
         <BoneMenu />
--- src/components/topPhotoRow/TopPhotoRow.js
+++ src/components/topPhotoRow/TopPhotoRow.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 import './TopPhotoRow.scss';
 
 class topPhotoRow extends Component {
-    render() {
-        return (
-            <div className="top-photo-row">
-                <p>Top Photo Row</p>
+  state = {
+    photos: [
+      "http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png",
+      "http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png",
+      "http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png",
+      "http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png",
+      "http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png",
+      "http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png",
+      "http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png",
+      "http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png"
+    ]
+  }
+  render() {
+    return (
+      <div className="top-photo-row">
+      {
+        this.state.photos.slice(0, this.props.photosCount).map( (photo, i) => {
+          return (
+            <div className="top-photo-row__img-container" key={i}>
+              <img className="top-photo-row__img-container__image"
+                src={photo}
+                alt="dog"></img>
             </div>
-        );
-    }
+          )
+        })
+      }
+      </div>
+    );
+  }
 }
 
 export default topPhotoRow;

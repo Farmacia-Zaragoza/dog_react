d9707e0575a1750be51c021be4c1a0778c0ac586
Mykola Lubenets <teabrutal@gmail.com>
2019-01-10 22:04:13 +0200

    Built

---
+++ docs/precache-manifest.23dcda837c57635d27a3a287e9c6d323.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff634594619954e12e6c",
+    "url": "/static/css/main.3a273ba8.chunk.css"
+  },
+  {
+    "revision": "ff634594619954e12e6c",
+    "url": "/static/js/main.ff634594.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e663006681ac99a5abf595596becd4e",
+    "url": "/index.html"
+  }
+];
